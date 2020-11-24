358a4f5623948ed58259e05413aa4ddb91ec5f43
morocco152 <58355129+morocco152@users.noreply.github.com>
2020-11-23 21:50:38 -0500

    Add files via upload

---
+++ Store_bd/login.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <title>LOGIN</title>
+        <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@600&display=swap" rel="stylesheet">
+        <link rel = "stylesheet" href = "assets/css/login.css">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    <header>
+        <strong><a href = "index.php" > TEMPLATE STORE </a></strong>
+    </header>
+            <form action = "login.php" method = "post">
+            <h1>LOGIN</h1>
+            <input type = "text" name = "email" placeholder = "email" require >
+            <input type = "password" name = "password" placeholder = "password" require >
+            <input type = "submit" value = "ENTER">
+    </body>
+</html>
